b85196facbe32f726640bacb703950143fa59b63
Chris <chrislee8973@Chriss-MacBook-Pro.local>
2016-12-01 04:22:48 -0500

    Hello World!

---
+++ hello-world/hello-world.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  cout << "Hello World!" << endl;
+  return 0;
+}
+
+//see more on
+//http://www.tutorialspoint.com/compile_cpp11_online.php
